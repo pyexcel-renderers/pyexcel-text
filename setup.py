11b4ae69d317e90c7aefc560216a491457ac9324
chfw <wangc_2011@hotmail.com>
2016-09-19 22:42:21 +0100

    sync wity pyexcel commons

--- setup.py
+++ setup.py
@@ -4,13 +4,15 @@ except ImportError:
     from ez_setup import use_setuptools
     use_setuptools()
     from setuptools import setup, find_packages
+import sys
+PY2 = sys.version_info[0] == 2
+PY26 = PY2 and sys.version_info[1] < 7
 
 NAME = 'pyexcel-text'
 AUTHOR = 'C.W.'
 VERSION = '0.2.4'
 EMAIL = 'wangc_2011 (at) hotmail.com'
 LICENSE = 'New BSD'
-PACKAGES = find_packages(exclude=['ez_setup', 'examples', 'tests'])
 DESCRIPTION = (
     'A plugin to pyexcel and provides the capability to present and write d' +
     'ata in text formats' +
@@ -33,14 +35,6 @@ KEYWORDS = [
     "json"
 ]
 
-INSTALL_REQUIRES = [
-    'pyexcel>=0.2.2',
-    'tabulate>=0.7.4',
-]
-
-EXTRAS_REQUIRE = {
-}
-
 CLASSIFIERS = [
     'Topic :: Office/Business',
     'Topic :: Utilities',
@@ -55,19 +49,52 @@ CLASSIFIERS = [
     'Programming Language :: Python :: Implementation :: PyPy'
 ]
 
+INSTALL_REQUIRES = [
+    'pyexcel>=0.2.2',
+    'tabulate>=0.7.4',
+]
+
+
+PACKAGES = find_packages(exclude=['ez_setup', 'examples', 'tests'])
+EXTRAS_REQUIRE = {
+}
+
 
 def read_files(*files):
     """Read files into setup"""
     text = ""
     for single_file in files:
-        text = text + read(single_file) + "\n"
+        content = read(single_file)
+        text = text + content + "\n"
     return text
 
 
 def read(afile):
     """Read a file into setup"""
     with open(afile, 'r') as opened_file:
-        return opened_file.read()
+        content = filter_out_test_code(opened_file)
+        content = "".join(list(content))
+        return content
+
+
+def filter_out_test_code(file_handle):
+    found_test_code = False
+    for line in file_handle.readlines():
+        if line.startswith('.. testcode:'):
+            found_test_code = True
+            continue
+        if found_test_code is True:
+            if line.startswith('  '):
+                continue
+            else:
+                empty_line = line.strip()
+                if len(empty_line) == 0:
+                    continue
+                else:
+                    found_test_code = False
+                    yield line
+        else:
+            yield line
 
 
 if __name__ == '__main__':
@@ -77,14 +104,14 @@ if __name__ == '__main__':
         version=VERSION,
         author_email=EMAIL,
         description=DESCRIPTION,
-        install_requires=INSTALL_REQUIRES,
+        long_description=read_files('README.rst', 'CHANGELOG.rst'),
+        license=LICENSE,
         keywords=KEYWORDS,
         extras_require=EXTRAS_REQUIRE,
+        tests_require=['nose'],
+        install_requires=INSTALL_REQUIRES,
         packages=PACKAGES,
         include_package_data=True,
-        long_description=read_files('README.rst', 'CHANGELOG.rst'),
         zip_safe=False,
-        tests_require=['nose'],
-        license=LICENSE,
         classifiers=CLASSIFIERS
     )
--- test.bat
+++ test.bat
@@ -1,3 +1,4 @@
 
+
 pip freeze
-nosetests --with-cov --cover-package pyexcel_text --cover-package tests --with-doctest --doctest-extension=.rst tests README.rst pyexcel_text  && flake8 . --exclude=.moban.d 
+nosetests --with-cov --cover-package pyexcel_text --cover-package tests --with-doctest --doctest-extension=.rst tests README.rst pyexcel_text && flake8 . --exclude=.moban.d --builtins=unicode,xrange,long
