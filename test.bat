0f3c43806b01b37ac8bc608c986454d45d6f0a73
chfw <wangc_2011@hotmail.com>
2016-10-25 09:13:18 +0100

    fix test failures

--- test.bat
+++ test.bat
@@ -1,4 +1,4 @@
 
 
 pip freeze
-nosetests --with-cov --cover-package pyexcel_text --cover-package tests --with-doctest --doctest-extension=.rst tests README.rst docs/source pyexcel_text && flake8 . --exclude=.moban.d --builtins=unicode,xrange,long
+nosetests --with-cov --cover-package pyexcel_text --cover-package tests --with-doctest --doctest-extension=.rst tests README.rst  pyexcel_text && flake8 . --exclude=.moban.d --builtins=unicode,xrange,long
